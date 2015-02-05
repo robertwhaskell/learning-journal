1fba025f142ce13118f4e457b2471f6de881b6f0
Robert Haskell <robertwhaskell@gmail.com>
2015-02-04 19:19:30 -0800

    started implementing fixtures, which I don not 100% get. More like 30%.

---
+++ test_journal.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from contextlib import closing
+from pyramid import testing
+import pytest
+
+from journal import connect_db
+from journal import DB_SCHEMA
+
+
+TEST_DSN = 'dbname=test_learning_journal user=roberthaskell'
+
+
+def init_db(settings):
+    with closing(connect_db(settings)) as db:
+        db.cursor().execute(DB_SCHEMA)
+        db.commit()
+
+
+def clear_db(settings):
+    with closing(connect_db(settings)) as db:
+        db.cursor().execute("DROP TABLE entries")
+        db.commit()
+
+
+def clear_entries(settings):
+    with closing(connect_db(settings)) as db:
+        db.cursor().execute("DELETE FROM entries")
+        db.commit()
+
+
+def run_query(db, query, params=(), get_results=True):
+    cursor = db.cursor()
+    cursor.execute(query, params)
+    db.commit()
+    results = None
+    if get_results:
+        results = cursor.fetchall()
+    return results
+
+
+@pytest.fixture(scope='session')
+def db(request):
+    """set up and tear down a database"""
+    settings = {'db': TEST_DSN}
+    init_db(settings)
+
+    def cleanup():
+        clear_db(settings)
+
+    request.addfinalizer(cleanup)
+
+    return settings
