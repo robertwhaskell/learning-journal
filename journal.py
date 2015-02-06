2dca266e9ed1a38992e0a7a7f1c6cf0578febe3f
Robert Haskell <robertwhaskell@gmail.com>
2015-02-06 11:37:30 -0800

    Making sure that everything is pushed properly

--- journal.py
+++ journal.py
@@ -10,12 +10,6 @@ from pyramid.view import view_config
 from waitress import serve
 from contextlib import closing
 from pyramid.httpexceptions import HTTPFound, HTTPInternalServerError
-from pyramid.authentication import AuthTktAuthenticationPolicy
-from pyramid.authorization import ACLAuthorizationPolicy
-from cryptacular.bcrypt import BCRYPTPasswordManager
-from pyramid.security import remember, forget
-here = os.path.dirname(os.path.abspath(__file__))
-
 
 DB_SCHEMA = """
 CREATE TABLE IF NOT EXISTS entries (
@@ -105,45 +99,6 @@ def read_entries(request):
     return {'entries': entries}
 
 
-def do_login(request):
-    username = request.params.get('username', None)
-    password = request.params.get('password', None)
-    if not (username and password):
-        raise ValueError('both username and password are required')
-
-    settings = request.registry.settings
-    manager = BCRYPTPasswordManager()
-    if username == settings.get('auth.username', ''):
-        hashed = settings.get('auth.password', '')
-        return manager.check(hashed, password)
-
-
-@view_config(route_name='login', renderer="templates/login.jinja2")
-def login(request):
-    """authenticate a user by username/password"""
-    username = request.params.get('username', '')
-    error = ''
-    if request.method == 'POST':
-        error = "Login Failed"
-        authenticated = False
-        try:
-            authenticated = do_login(request)
-        except ValueError as e:
-            error = str(e)
-
-        if authenticated:
-            headers = remember(request, username)
-            return HTTPFound(request.route_url('home'), headers=headers)
-
-    return {'error': error, 'username': username}
-
-
-@view_config(route_name='logout')
-def logout(request):
-    headers = forget(request)
-    return HTTPFound(request.route_url('home'), headers=headers)
-
-
 def main():
     """Create a configured wsgi app"""
     settings = {}
@@ -152,32 +107,18 @@ def main():
     settings['db'] = os.environ.get(
         'DATABASE_URL', 'dbname=learning_journal user=roberthaskell'
         )
-    settings['auth.username'] = os.environ.get('AUTH_USERNAME', 'admin')
-    manager = BCRYPTPasswordManager()
-    settings['auth.password'] = os.environ.get(
-        'AUTH_PASSWORD', manager.encode('secret')
-    )
     # secret value for session signing:
     secret = os.environ.get('JOURNAL_SESSION_SECRET', 'itsaseekrit')
     session_factory = SignedCookieSessionFactory(secret)
-    auth_secret = os.environ.get('JOURNAL_AUTH_SECRET', 'anotherseekrit')
     # configuration setup
     config = Configurator(
         settings=settings,
-        session_factory=session_factory,
-        authentication_policy=AuthTktAuthenticationPolicy(
-            secret=auth_secret,
-            hashalg='sha512'
-        ),
-        authorization_policy=ACLAuthorizationPolicy(),
-
+        session_factory=session_factory
     )
     config.include('pyramid_jinja2')
-    config.add_static_view('static', os.path.join(here, 'static'))
+    config.add_route('home', '/')
     config.add_route('home', '/')
     config.add_route('add', '/add')
-    config.add_route('login', '/login')
-    config.add_route('logout', '/logout')
     config.scan()
     app = config.make_wsgi_app()
     return app
--- test_journal.py
+++ test_journal.py
@@ -7,7 +7,6 @@ import os
 from journal import INSERT_ENTRY
 from journal import connect_db
 from journal import DB_SCHEMA
-from cryptacular.bcrypt import BCRYPTPasswordManager
 
 
 TEST_DSN = 'dbname=test_learning_journal user=roberthaskell'
@@ -169,94 +168,4 @@ def test_post_to_add_view(app):
     actual = redirected.body
     for expected in entry_data.values():
         assert expected in actual
-
-
-@pytest.fixture(scope='function')
-def auth_req(request):
-    manager = BCRYPTPasswordManager()
-    settings = {
-        'auth.username': 'admin',
-        'auth.password': manager.encode('secret'),
-    }
-    testing.setUp(settings=settings)
-    req = testing.DummyRequest()
-
-    def cleanup():
-        testing.tearDown()
-
-    request.addfinalizer(cleanup)
-
-    return req
-
-
-def test_do_login_success(auth_req):
-    from journal import do_login
-    auth_req.params = {'username': 'admin', 'password': 'secret'}
-    assert do_login(auth_req)
-
-
-def test_do_login_bad_pass(auth_req):
-    from journal import do_login
-    auth_req.params = {'username': 'admin', 'password': 'wrong'}
-    assert not do_login(auth_req)
-
-
-def test_do_login_bad_user(auth_req):
-    from journal import do_login
-    auth_req.params = {'username': 'bad', 'password': 'secret'}
-    assert not do_login(auth_req)
-
-
-def test_do_login_missing_params(auth_req):
-    from journal import do_login
-    for params in ({'username': 'admin'}, {'password': 'secret'}):
-        auth_req.params = params
-        with pytest.raises(ValueError):
-            do_login(auth_req)
-
-
-INPUT_BTN = '<input type="submit" value="Share" name="Share"/>'
-
-
-def login_helper(username, password, app):
-    """encapsulate app login for reuse in tests
-
-    Accept all status codes so that we can make assertions in tests
-    """
-    login_data = {'username': username, 'password': password}
-    return app.post('/login', params=login_data, status='*')
-
-
-def test_start_as_anonymous(app):
-    response = app.get('/', status=200)
-    actual = response.body
-    assert INPUT_BTN not in actual
-
-
-def test_login_success(app):
-    username, password = ('admin', 'secret')
-    redirect = login_helper(username, password, app)
-    assert redirect.status_code == 302
-    response = redirect.follow()
-    assert response.status_code == 200
-    actual = response.body
-    assert INPUT_BTN in actual
-
-
-def test_login_fails(app):
-    username, password = ('admin', 'wrong')
-    response = login_helper(username, password, app)
-    assert response.status_code == 200
-    actual = response.body
-    assert "Login Failed" in actual
-    assert INPUT_BTN not in actual
-
-
-def test_logout(app):
-    # re-use existing code to ensure we are logged in when we begin
-    test_login_success(app)
-    redirect = app.get('/logout', status="3*")
-    response = redirect.follow()
-    assert response.status_code == 200
-    actual = response.body
-    assert INPUT_BTN not in actual
+    
