6549312351ca1155b56dbcdbdf53c703093a220a
Robert Haskell <robertwhaskell@gmail.com>
2015-02-14 13:09:21 -0800

    working on lettuce tests. Finished with the editing test, which also functions well as the detail test.

---
+++ features/edit.feature
@@ -0,0 +1,11 @@
+Feature: Edit Features
+
+
+    Scenario Outline: checking to see if things are edited correctly
+        When logged in
+        Go to the home page
+        Click the detail button of an entry
+        Then I see the entries text and title on the detail page
+        Click the edit button on the detail page
+        Edit the entry
+        Then I see the change on the home page
--- features/steps.py
+++ features/steps.py
@@ -36,7 +36,7 @@ def app():
 def enter_data(step):
     """populate the database"""
     entry('Test Title', 'Test Text')
-    entry('Uneditted Title', 'Unedited Text')
+    entry('Unedited Title', 'Unedited Text')
     entry('Markdown Test', '### Header3')
     mkdn = """
     ```python
@@ -99,6 +99,57 @@ def confirm_title_on_homepage(step, page, title):
     world.page = page
     assert title in world.app.get(page)
 
+
 @step('I see that it is a markdown entry')
 def confirm_markdown(step):
     assert markdown.markdown('Test Text', extensions=('codehilite', 'fenced_code')) in world.app.get(world.page)
+
+
+@step('logged in')
+def log_in(step):
+    world.homepage = world.app.get('/')
+    world.loginpage = world.homepage.click(linkid='login')
+    f = world.loginpage.form
+    f['username'] = 'admin'
+    f['password'] = 'secret'
+    f.submit('submit')
+
+
+@step('Go to the home page')
+def Go_to_home_page(step):
+    world.homepage = world.app.get('/')
+    assert '<h2>Entries</h2>' in world.homepage
+
+
+@step('Click the detail button of an entry')
+def click_detail(step):
+    world.detailpage = world.homepage.click('Detail', linkid='2')
+    assert 'Details' in world.detailpage
+
+
+@step('Then I see the entries text and title on the detail page')
+def confirm_entry(step):
+    assert 'Unedited Title' in world.detailpage
+    assert 'Unedited Text' in world.detailpage
+
+
+@step('Click the edit button on the detail page')
+def click_edit(step):
+    assert 'Edit' in world.detailpage
+    world.editpage = world.detailpage.click('Edit')
+    assert 'Edit' in world.editpage
+
+
+@step('Edit the entry')
+def edit_entry(step):
+    f = world.editpage.form
+    f['title'] = 'Edited Title'
+    f['text'] = 'Edited Text'
+    f.submit('submit')
+
+
+@step('Then I see the change on the home page')
+def confirm_change(step):
+    world.homepage = world.app.get('/')
+    assert 'Edited Title' in world.homepage
+    assert 'Edited Text' in world.homepage
